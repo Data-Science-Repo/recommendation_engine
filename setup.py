9151ef27ed7084c6d1e364cbcdb675ef53076b24
Singh Vivek <Singh.Vivek@bcg.com>
2020-01-25 14:53:30 +0800

    uploading repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A brief repo for building recommendation engine',
+    author='singh vivek',
+    license='MIT',
+)
